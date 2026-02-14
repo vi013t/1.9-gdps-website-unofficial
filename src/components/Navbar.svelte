2dda1b6947cc5c66674c2ecfb42d245f59f96971
Violet Iapalucci <violet_iapalucci@outlook.com>
2026-02-14 17:44:46 -0500

    fuck ai dude

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -32,8 +32,8 @@
 		</button>
 	{:else}
 		<div class="no-ai" onmouseenter={() => showAiTooltip = true} onmouseleave={() => showAiTooltip = false} role="tooltip">
-			<RobotIcon stroke="black" style="width: 1.5em; height: 1.5em; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);" />
-			<NotAllowedIcon stroke="red" style="width: 1.5em; height: 1.5em; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);" />
+			<RobotIcon stroke="black" style="width: 1.5em; height: 1.5em; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -55%);" />
+			<NotAllowedIcon stroke="red" style="width: 1.5em; height: 1.5em; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -45%);" />
 			<Tooltip bind:visible={showAiTooltip}>No AI was used in the making of this website.</Tooltip>
 		</div>
 
